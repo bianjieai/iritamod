133e070f007836e322125312a2c1d5e545f32ca7
secret <secret@bianjie.ai>
2020-11-28 22:32:28 +0800

    add type flag for genkey

--- modules/genutil/client/cli/genkey.go
+++ modules/genutil/client/cli/genkey.go
@@ -1,9 +1,13 @@
 package cli
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
+	"github.com/tendermint/tendermint/crypto"
 	"github.com/tendermint/tendermint/libs/cli"
 	"github.com/tendermint/tendermint/libs/tempfile"
 
@@ -13,26 +17,36 @@ import (
 )
 
 var (
+	FlagType    = "type"
 	FlagOutFile = "out-file"
 )
 
-// Genkey returns a command that generates the key from priv_validator_key.json.
+// Genkey returns a command that generates the key from priv_validator_key.json or node_key.json
 // Will used to generate CA request.
 func GenKey(defaultNodeHome string) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "genkey",
-		Short: "generate key from validator key file",
-		Args:  cobra.NoArgs,
+		Use:     "genkey",
+		Short:   "generate key from the key file",
+		Args:    cobra.NoArgs,
+		PreRunE: preCheckCmd,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			serverCtx := server.GetServerContextFromCmd(cmd)
 			config := serverCtx.Config
 
-			_, filePv, err := genutil.InitializeNodeValidatorFiles(config)
+			nodeKey, filePv, err := genutil.InitializeNodeValidatorFiles(config)
 			if err != nil {
 				return err
 			}
 
-			privKey := filePv.Key.PrivKey
+			var privKey crypto.PrivKey
+
+			keyType := strings.TrimSpace(viper.GetString(FlagType))
+			if keyType == "node" {
+				privKey = nodeKey.PrivKey
+			} else {
+				privKey = filePv.Key.PrivKey
+			}
+
 			key, err := genutil.Genkey(privKey)
 			if err != nil {
 				return err
@@ -42,8 +56,22 @@ func GenKey(defaultNodeHome string) *cobra.Command {
 		},
 	}
 
+	cmd.Flags().String(FlagType, "validator", "key type (node|validator)")
 	cmd.Flags().String(cli.HomeFlag, defaultNodeHome, "node's home directory")
 	cmd.Flags().String(FlagOutFile, "priv.pem", "private key file path")
 
 	return cmd
+}
+
+func preCheckCmd(cmd *cobra.Command, _ []string) error {
+	flags := cmd.Flags()
+
+	if flags.Changed(FlagType) {
+		keyType := strings.TrimSpace(viper.GetString(FlagType))
+		if keyType != "node" && keyType != "validator" {
+			return fmt.Errorf("key type must be node or validator")
+		}
+	}
+
+	return nil
 }
--- modules/genutil/utils.go
+++ modules/genutil/utils.go
@@ -43,23 +43,21 @@ func ExportGenesisFileWithTime(
 }
 
 // InitializeNodeValidatorFiles creates private validator and p2p configuration files.
-func InitializeNodeValidatorFiles(config *cfg.Config) (nodeID string, pv *privval.FilePV, err error) {
-	nodeKey, err := p2p.LoadOrGenNodeKey(config.NodeKeyFile())
+func InitializeNodeValidatorFiles(config *cfg.Config) (nodeKey *p2p.NodeKey, pv *privval.FilePV, err error) {
+	nodeKey, err = p2p.LoadOrGenNodeKey(config.NodeKeyFile())
 	if err != nil {
-		return "", nil, err
+		return nil, nil, err
 	}
-
-	nodeID = string(nodeKey.ID())
 
 	pvKeyFile := config.PrivValidatorKeyFile()
 	if err := tmos.EnsureDir(filepath.Dir(pvKeyFile), 0777); err != nil {
-		return "", nil, err
+		return nil, nil, err
 	}
 
 	pvStateFile := config.PrivValidatorStateFile()
 	if err := tmos.EnsureDir(filepath.Dir(pvStateFile), 0777); err != nil {
-		return "", nil, err
+		return nil, nil, err
 	}
 
-	return nodeID, privval.LoadOrGenFilePV(pvKeyFile, pvStateFile), nil
+	return nodeKey, privval.LoadOrGenFilePV(pvKeyFile, pvStateFile), nil
 }
