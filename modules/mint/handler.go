d9d5406dba7571acd80e1bc2a205a3bef56a3acf
secret <secret@bianjie.ai>
2021-01-20 09:45:55 +0800

    add handler

---
+++ modules/mint/handler.go
@@ -0,0 +1,26 @@
+package mint
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
+	"github.com/bianjieai/iritamod/modules/mint/keeper"
+)
+
+// NewHandler returns the message handler for mint module
+func NewHandler(k Keeper) sdk.Handler {
+	msgServer := keeper.NewMsgServerImpl(k)
+
+	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
+		ctx = ctx.WithEventManager(sdk.NewEventManager())
+
+		switch msg := msg.(type) {
+		case *MsgMint:
+			res, err := msgServer.Mint(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+
+		default:
+			return nil, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "unrecognized %s message type: %T", ModuleName, msg)
+		}
+	}
+}
