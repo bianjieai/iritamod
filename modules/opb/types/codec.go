6fdbc68a3c02d8d7f9fef397f2df9c704f12ec83
yuandu <taramaoriskykage@gmail.com>
2023-06-14 16:13:58 +0800

    refactor: opb params

--- modules/opb/types/codec.go
+++ modules/opb/types/codec.go
@@ -24,12 +24,14 @@ func init() {
 func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
 	cdc.RegisterConcrete(&MsgMint{}, "irita/opb/MsgMint", nil)
 	cdc.RegisterConcrete(&MsgReclaim{}, "irita/opb/MsgReclaim", nil)
+	cdc.RegisterConcrete(&MsgUpdateParams{}, "irita/opb/MsgUpdateParams", nil)
 }
 
 func RegisterInterfaces(registry types.InterfaceRegistry) {
 	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgMint{},
 		&MsgReclaim{},
+		&MsgUpdateParams{},
 	)
 
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
