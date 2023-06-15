4913e76406e8197c18271a755ef2ddfca7ca57ba
yuandu <taramaoriskykage@gmail.com>
2023-06-15 15:10:04 +0800

    chore: add one missing

--- modules/params/module.go
+++ modules/params/module.go
@@ -112,7 +112,7 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 }
 
 // ConsensusVersion implements AppModule/ConsensusVersion.
-func (AppModule) ConsensusVersion() uint64 { return 1 }
+func (AppModule) ConsensusVersion() uint64 { return ConsensusVersion }
 
 // BeginBlock returns the begin blocker for the params module.
 func (am AppModule) BeginBlock(ctx sdk.Context, _ abci.RequestBeginBlock) {}
