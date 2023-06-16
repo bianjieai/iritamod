665435aacdf768e8a8a6c4c9f40d367699c9410c
yuandu <taramaoriskykage@gmail.com>
2023-06-16 18:04:43 +0800

    feat: cparam tx

--- modules/params/client/cli/tx.go
+++ modules/params/client/cli/tx.go
@@ -1,61 +1,126 @@
 package cli
 
-//import (
-//	"github.com/spf13/cobra"
-//
-//	"github.com/cosmos/cosmos-sdk/client"
-//	"github.com/cosmos/cosmos-sdk/client/flags"
-//	"github.com/cosmos/cosmos-sdk/client/tx"
-//
-//	"github.com/bianjieai/iritamod/modules/params/client/utils"
-//	"github.com/bianjieai/iritamod/modules/params/types"
-//)
-//
-//func NewTxCmd() *cobra.Command {
-//	paramsTxCmd := &cobra.Command{
-//		Use:                        types.ModuleName,
-//		Short:                      "Params transaction subcommands",
-//		DisableFlagParsing:         true,
-//		SuggestionsMinimumDistance: 2,
-//		RunE:                       client.ValidateCmd,
-//	}
-//
-//	paramsTxCmd.AddCommand(
-//		NewUpdateParamsCmd(),
-//	)
-//
-//	return paramsTxCmd
-//}
-//
-//// NewUpdateParamsCmd implements the update params command handler.
-//func NewUpdateParamsCmd() *cobra.Command {
-//	cmd := &cobra.Command{
-//		Use:   "update [change-file]",
-//		Short: "UpdateValidator params",
-//		Args:  cobra.ExactArgs(1),
-//		RunE: func(cmd *cobra.Command, args []string) error {
-//			clientCtx, err := client.GetClientTxContext(cmd)
-//			if err != nil {
-//				return err
-//			}
-//
-//			changes, _ := utils.ParseParamChange(clientCtx.LegacyAmino, args[0])
-//
-//			msg := types.NewMsgUpdateParams(
-//				changes.ToParamChanges(),
-//				clientCtx.GetFromAddress(),
-//			)
-//
-//			if err := msg.ValidateBasic(); err != nil {
-//				return err
-//			}
-//
-//			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
-//		},
-//	}
-//
-//	flags.AddTxFlagsToCmd(cmd)
-//	_ = cmd.MarkFlagRequired(flags.FlagFrom)
-//
-//	return cmd
-//}
+import (
+	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/manifoldco/promptui"
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/bianjieai/iritamod/modules/params/types"
+)
+
+func NewTxCmd() *cobra.Command {
+	paramsTxCmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      "Params transaction subcommands",
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	paramsTxCmd.AddCommand(
+		NewCmdUpdateParams(),
+		NewDraftUpdateParams(),
+	)
+
+	return paramsTxCmd
+}
+
+// NewCmdUpdateParams implements the update params command handler.
+func NewCmdUpdateParams() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "update [params.json]",
+		Short: "update module params",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			msgs, err := parseUpdateParamsFile(clientCtx.Codec, args[0])
+			if err != nil {
+				return err
+			}
+
+			msg, err := types.NewMsgUpdateParams(msgs, clientCtx.GetFromAddress().String())
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+	_ = cmd.MarkFlagRequired(flags.FlagFrom)
+
+	return cmd
+}
+
+func NewDraftUpdateParams() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "draft",
+		Short: "Generate a draft update params file",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			var updateParam updateParamsType
+			// select available update params message
+			msgPrompt := promptui.Select{
+				Label: "Select update params message type",
+				Items: func() []string {
+					umsgs := make([]string, 0)
+					msgs := clientCtx.InterfaceRegistry.ListImplementations(sdk.MsgInterfaceProtoName)
+					// filter only update params msg types out
+					for _, msg := range msgs {
+						if strings.HasSuffix(msg, types.ParamsMsgSuffix) && msg != types.ParamsMsgTypeURL {
+							umsgs = append(umsgs, msg)
+						}
+					}
+					sort.Strings(umsgs)
+					return umsgs
+				},
+			}
+
+			_, result, err := msgPrompt.Run()
+			if err != nil {
+				return fmt.Errorf("failed to prompt update params types: %w", err)
+			}
+
+			updateParam.MsgType = result
+
+			if updateParam.MsgType != "" {
+				updateParam.Msg, err = sdk.GetMsgFromTypeURL(clientCtx.Codec, updateParam.MsgType)
+				if err != nil {
+					// should never happen
+					panic(err)
+				}
+			}
+
+			res, err := updateParam.Prompt(clientCtx.Codec)
+			if err != nil {
+				return err
+			}
+
+			if err := writeFile(draftUpdateParamsFileName, res); err != nil {
+				return err
+			}
+
+			fmt.Printf("The draft update params file has successfully been generated.")
+
+			return nil
+
+			return nil
+		},
+	}
+
+	return cmd
+}
