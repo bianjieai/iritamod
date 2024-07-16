ae5f4e4f73c71ae8a5cd7235e934d3ff3f67ff59
shiwei <15652931283@163.com>
2024-07-16 16:18:38 +0800

    update upgrade

--- modules/upgrade/keeper/keeper.go
+++ modules/upgrade/keeper/keeper.go
@@ -1,8 +1,11 @@
 package keeper
 
 import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	xp "github.com/cosmos/cosmos-sdk/x/upgrade/exported"
 	upgradekeeper "github.com/cosmos/cosmos-sdk/x/upgrade/keeper"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
@@ -10,18 +13,18 @@ import (
 )
 
 type Keeper struct {
-	uk *upgradekeeper.Keeper
+	*upgradekeeper.Keeper
 }
 
-func NewKeeper(uk *upgradekeeper.Keeper) Keeper {
+func NewKeeper(skipUpgradeHeights map[int64]bool, storeKey storetypes.StoreKey, cdc codec.BinaryCodec, homePath string, vs xp.ProtocolVersionSetter, authority string) Keeper {
 	return Keeper{
-		uk: uk,
+		upgradekeeper.NewKeeper(skipUpgradeHeights, storeKey, cdc, homePath, vs, authority),
 	}
 }
 
 // ScheduleUpgrade schedules an upgrade based on the specified plan.
 func (k Keeper) ScheduleUpgrade(ctx sdk.Context, msg *types.MsgUpgradeSoftware) error {
-	p, has := k.uk.GetUpgradePlan(ctx)
+	p, has := k.GetUpgradePlan(ctx)
 	if has {
 		return sdkerrors.Wrapf(
 			sdkerrors.ErrInvalidRequest,
@@ -35,29 +38,29 @@ func (k Keeper) ScheduleUpgrade(ctx sdk.Context, msg *types.MsgUpgradeSoftware) 
 		Height: msg.Height,
 		Info:   msg.Info,
 	}
-	return k.uk.ScheduleUpgrade(ctx, plan)
+	return k.Keeper.ScheduleUpgrade(ctx, plan)
 }
 
 // ClearUpgradePlan clears currently schedule upgrade
 func (k Keeper) ClearUpgradePlan(ctx sdk.Context) error {
-	_, has := k.uk.GetUpgradePlan(ctx)
+	_, has := k.GetUpgradePlan(ctx)
 	if !has {
 		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "there is currently no upgrade plan")
 	}
 
-	k.uk.ClearUpgradePlan(ctx)
+	k.Keeper.ClearUpgradePlan(ctx)
 	return nil
 }
 
 func (k Keeper) UpgradeKeeper() *upgradekeeper.Keeper {
-	return k.uk
+	return k.Keeper
 }
 
 // SetUpgradeHandler sets an UpgradeHandler for the upgrade specified by name. This handler will be called when the upgrade
 // with this name is applied. In order for an upgrade with the given name to proceed, a handler for this upgrade
 // must be set even if it is a no-op function.
 func (k *Keeper) SetUpgradeHandler(name string, upgradeHandler upgradetypes.UpgradeHandler) {
-	k.uk.SetUpgradeHandler(name, upgradeHandler)
+	k.Keeper.SetUpgradeHandler(name, upgradeHandler)
 }
 
 // ReadUpgradeInfoFromDisk returns the name and height of the upgrade
@@ -65,10 +68,10 @@ func (k *Keeper) SetUpgradeHandler(name string, upgradeHandler upgradetypes.Upgr
 // if there's an error in reading the info,
 // it assumes that the upgrade info is not available
 func (k Keeper) ReadUpgradeInfoFromDisk() (upgradetypes.Plan, error) {
-	return k.uk.ReadUpgradeInfoFromDisk()
+	return k.Keeper.ReadUpgradeInfoFromDisk()
 }
 
 // IsSkipHeight checks if the given height is part of skipUpgradeHeights
 func (k Keeper) IsSkipHeight(height int64) bool {
-	return k.uk.IsSkipHeight(height)
+	return k.Keeper.IsSkipHeight(height)
 }
