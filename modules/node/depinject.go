0aaf46030dc13cc4fae42033c926c0f6c1a148f0
shiwei <15652931283@163.com>
2024-07-24 13:45:32 +0800

    update

--- modules/node/depinject.go
+++ modules/node/depinject.go
@@ -6,6 +6,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	store "github.com/cosmos/cosmos-sdk/store/types"
 	cosmosparamstypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	staking "github.com/cosmos/cosmos-sdk/x/staking/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	paramskeeper "iritamod.bianjie.ai/modules/params/keeper"
 
 	modulev1 "iritamod.bianjie.ai/api/iritamod/node/module/v1"
@@ -16,17 +18,30 @@ import (
 // App Wiring Setup
 func init() {
 	appmodule.Register(&modulev1.Module{},
+		// appmodule.Provide(ProvideModule, ProvideKeyTable),
 		appmodule.Provide(ProvideModule),
+		appmodule.Invoke(InvokeHooks),
 	)
 }
 
 var _ appmodule.AppModule = AppModule{}
+
+//func ProvideKeyTable() paramstypes.KeyTable {
+//	return keeper.ParamKeyTable()
+//}
 
 // IsOnePerModuleType implements the depinject.OnePerModuleType interface.
 func (am AppModule) IsOnePerModuleType() {}
 
 // IsAppModule implements the appmodule.AppModule interface.
 func (am AppModule) IsAppModule() {}
+
+type HookInputs struct {
+	depinject.In
+
+	Hooks  []staking.StakingHooks `optional:"true"`
+	Keeper *keeper.Keeper
+}
 
 type NodeInputs struct {
 	depinject.In
@@ -54,7 +69,11 @@ func ProvideModule(in NodeInputs) NodeOutputs {
 		in.Key,
 		subspace,
 	)
-	m := NewAppModule(in.Cdc, nodekeeper)
+	//nodekeeper.SetHooks(in.StakingKeeper.Hooks())
+	m := NewAppModule(in.Cdc, *nodekeeper)
 
-	return NodeOutputs{NodeKeeper: &nodekeeper, Module: m}
+	return NodeOutputs{NodeKeeper: nodekeeper, Module: m}
+}
+func InvokeHooks(in HookInputs) {
+	in.Keeper.SetHooks(stakingtypes.NewMultiStakingHooks(in.Hooks...))
 }
